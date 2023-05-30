efc4549d5909475fc078c6bb8953a8a0044c459f
Amine Jegani <amine.jegani@stud.htwk-leipzig.de>
2023-05-30 19:31:04 +0200

    Erstes Design des Frontends realisieren

---
+++ src/app/sound-box/sound-box.component.ts
@@ -0,0 +1,20 @@
+
+
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-sound-box',
+  templateUrl: './sound-box.component.html',
+  styleUrls: ['./sound-box.component.css']
+})
+export class SoundBoxComponent {
+  constructor(private router: Router) { }
+
+  handleButtonClick() {
+    console.log("hallo");
+    this.router.navigate(['/weitereSeite']);
+  }
+}
+
+
