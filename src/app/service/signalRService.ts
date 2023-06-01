716b981744eeace37c5c87c4f6a07614ad82d935
Luca <luca_niklas.franke@stud.htwk-leipzig.de>
2023-06-01 15:56:37 +0200

    #11 feat: add signalRService.ts, establish connection with backend

---
+++ src/app/service/signalRService.ts
@@ -0,0 +1,26 @@
+import { Injectable } from '@angular/core';
+import * as signalR from '@microsoft/signalr';
+import { Subject } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class SignalRService {
+  private hubConnection: signalR.HubConnection;
+  public receivedMessage: Subject<string> = new Subject<string>();
+
+  constructor() {
+
+    this.hubConnection = new signalR.HubConnectionBuilder()
+      .withUrl('http://localhost:5003/communicationHub') // Specify the SignalR endpoint URL
+      .build();
+
+    this.hubConnection.start()
+      .then(() => console.log('SignalR connected.'))
+      .catch(err => console.error('SignalR connection error: ', err));
+
+    this.hubConnection.on('ReceiveMessage', (message: string) => {
+      this.receivedMessage.next(message); // Notify subscribers about received messages
+    });
+  }
+}
--- src/app/text-box/text-box.component.ts
+++ src/app/text-box/text-box.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {SignalRService} from "../service/signalRService";
 
 @Component({
   selector: 'app-text-box',
   templateUrl: './text-box.component.html',
   styleUrls: ['./text-box.component.css']
 })
-export class TextBoxComponent {
+export class TextBoxComponent implements OnInit {
+  receivedMessage = "";
+
+  constructor(private signalRService: SignalRService) {
+  }
+
+  ngOnInit() {
+    this.signalRService.receivedMessage.subscribe((message: string) => {
+      this.receivedMessage = message;
+    })
+  }
 
 }
