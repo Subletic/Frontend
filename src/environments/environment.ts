f09a408ce63b8ff77514779950991a8ec904f5df
Luca.Noack@stud.htwk-leipzig.de <Luca.Noack@stud.htwk-leipzig.de>
2023-06-25 13:44:04 +0200

    #112 feat: update enviroments file

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,7 @@
 export const environment = {
   production: true,
-  apiURL: 'http://localhost:40114'
+  // if you want to deploy your application please set the environment variable to:
+  // apiURL: 'http://fim-swe-proj-d:40115'
+  // if coding locally on your device please use the api apiURL:
+  apiURL: 'http://localhost:40115'
 };
