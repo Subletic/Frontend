33ec20185c7b979bd2e551723a894f42f428300b
Luca Noack <luca.noack@stud.htwk-leipzig.de>
2023-06-28 09:59:11 +0000

    #105 fix: updateWrongApi

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
   // if you want to deploy your application please set the environment variable to:
-  // apiURL: 'http://fim-swe-proj-d:40115'
+  // apiURL: 'http://fim-swe-proj-d:40114'
   // if coding locally on your device please use the api apiURL:
-  apiURL: 'http://localhost:40115'
+  apiURL: 'http://localhost:40114'
 };
