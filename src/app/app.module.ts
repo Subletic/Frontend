4bfd0aa6305c33a4837c9502bfc0cd420f108a15
Luca.Noack@stud.htwk-leipzig.de <Luca.Noack@stud.htwk-leipzig.de>
2023-07-06 15:23:46 +0200

    #8 fix: bugs

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,7 +8,12 @@ import { SoundBoxComponent } from './sound-box/sound-box.component';
 import { TextBoxComponent } from './text-box/text-box.component';
 import { TextSheetComponent } from './textSheet/textSheet.component';
 import { AudioHandlerComponent } from './audio-handler/audio-handler.component';
-
+import { SettingsComponent } from './settings/settings.component';
+import { MatButtonModule } from '@angular/material/button';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatIconModule } from '@angular/material/icon';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { MatSliderModule } from '@angular/material/slider';
 import { SliderPopupComponent } from './sound-box/slider-popup/slider-popup.component';
@@ -28,7 +33,12 @@ import { SliderPopupComponent } from './sound-box/slider-popup/slider-popup.comp
   imports: [
     BrowserModule,
     FormsModule,
-    MatSliderModule
+    MatButtonModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatIconModule,
+    MatSliderModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
