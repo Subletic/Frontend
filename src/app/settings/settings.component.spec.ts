2d5fc760381d0131dc740e56e1d4d8eccb62e0b0
Chantal Bley <chantal.bley@stud.htwk-leipzig.de>
2023-07-09 15:10:19 +0200

    #32 feat: change inputfield to slider, update unittests

--- src/app/settings/settings.component.spec.ts
+++ src/app/settings/settings.component.spec.ts
@@ -1,5 +1,6 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { SettingsComponent } from './settings.component';
+import { environment } from '../../environments/environment';
 import { MatIconModule } from '@angular/material/icon';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { FormsModule } from '@angular/forms';
@@ -23,42 +24,80 @@ describe('SettingsComponent', () => {
     fixture.detectChanges();
   });
 
-  it('should set "sprungweite" to 5 by default', () => {
-    expect(component.sprungweite).toBe(5);
+  it('should create', () => {
+    expect(component).toBeTruthy();
   });
 
-  it('should enable the "Ok" button when "sprungweite" is between 1 and 120', () => {
-    component.sprungweite = 60;
+  it('should initialize with default values', () => {
+    expect(component.id).toBeUndefined();
+    expect(component.seconds).toBe(5);
+    expect(component.initialSeconds).toBe(5);
+  });
+
+  it('should set the slider value when ngAfterViewInit is called', () => {
+    const sliderElement = fixture.nativeElement.querySelector('#seconds-slider');
+    component.seconds = 10;
     fixture.detectChanges();
 
-    const okButton: HTMLButtonElement = fixture.nativeElement.querySelector(
-      '.settings-button:first-child'
-    );
-    expect(okButton.disabled).toBeFalsy();
+    component.ngAfterViewInit();
+    expect(sliderElement.value).toBe('10');
   });
 
-  it('should emit the "secondsChange" event when "Ok" button is clicked', () => {
-    spyOn(component.secondsChange, 'emit');
-    component.sprungweite = 30;
-    fixture.detectChanges();
+  it('should open and close the modal', () => {
+    spyOn(component, 'open').and.callThrough();
+    spyOn(component, 'close').and.callThrough();
+    spyOn(document.body.classList, 'add');
+    spyOn(document.body.classList, 'remove');
 
-    const okButton: HTMLButtonElement = fixture.nativeElement.querySelector(
-      '.settings-button:first-child'
-    );
-    okButton.click();
+    component.open();
+    expect(component.open).toHaveBeenCalled();
+    expect(document.body.classList.add).toHaveBeenCalledWith('settings-open');
 
-    expect(component.secondsChange.emit).toHaveBeenCalledWith(30);
+    component.close();
+    expect(component.close).toHaveBeenCalled();
+    expect(document.body.classList.remove).toHaveBeenCalledWith('settings-open');
   });
 
-  it('should close the modal when "close" button is clicked', () => {
+  it('should cancel changes and reset seconds value', () => {
+    component.seconds = 10;
+    component.initialSeconds = 5;
+
     spyOn(component, 'close');
 
-    const closeButton: HTMLButtonElement = fixture.nativeElement.querySelector(
-      'button[mat-icon-button]'
-    );
-    closeButton.click();
-
+    component.cancel();
+    expect(component.seconds).toBe(5);
     expect(component.close).toHaveBeenCalled();
   });
 
+  it('should apply changes and emit secondsChange event', () => {
+    spyOn(component.secondsChange, 'emit');
+    spyOn(component, 'close');
+  
+    component.seconds = 10;
+  
+    component.apply();
+  
+    expect(component.secondsChange.emit).toHaveBeenCalledWith(10);
+    expect(component.close).toHaveBeenCalled();
+  });
+
+  const mockFetch = (status: number) => {
+    spyOn(window, 'fetch').and.returnValue(
+      Promise.resolve({
+        ok: status === 200,
+      } as Response)
+    );
+  };
+
+  it('should handle error when calling backend reload', async () => {
+    spyOn(window.console, 'error');
+    mockFetch(500);
+
+    component.callBackendReload();
+    expect(window.fetch).toHaveBeenCalledWith(environment.apiURL + '/api/restart', { method: 'POST' });
+
+    await new Promise((resolve) => setTimeout(resolve, 0));
+
+    expect(window.console.error).toHaveBeenCalledWith('Error with calling restart');
+  });
 });
