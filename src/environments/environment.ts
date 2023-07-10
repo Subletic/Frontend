4c5dd9df3062296995effc24b4f8b32dd2536648
Luca Noack <luca.noack@stud.htwk-leipzig.de>
2023-07-10 20:48:29 +0000

    #105 feat: server deployment

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
   // if you want to deploy your application please set the environment variable to:
-  // apiURL: 'http://fim-swe-proj-d:40114'
+  apiURL: 'http://fim-swe-proj-d:40114'
   // if coding locally on your device please use the api apiURL:
-  apiURL: 'http://localhost:40114'
+  // apiURL: 'http://localhost:40114'
 };
