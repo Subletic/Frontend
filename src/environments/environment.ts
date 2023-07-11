7a50970affcb8086a841d8740750a9c38cf070a0
Luca Noack <luca.noack@stud.htwk-leipzig.de>
2023-07-11 07:05:52 +0000

    #105 fix: link

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
   // if you want to deploy your application please set the environment variable to:
-  apiURL: 'http://d.projekte.swe.htwk-leipzig.de:40110/'
+  apiURL: 'http://d.projekte.swe.htwk-leipzig.de:40110'
   // if coding locally on your device please use the api apiURL:
   // apiURL: 'http://localhost:40114'
 };
