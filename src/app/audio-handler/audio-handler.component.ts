14d87d8de80d4730fbfd77c99c8d65e4240650c9
Luca <luca_niklas.franke@stud.htwk-leipzig.de>
2023-10-31 00:13:15 +0100

    #37 feat: add skipping forward/backwards back, fix pausing playback

--- src/app/audio-handler/audio-handler.component.ts
+++ src/app/audio-handler/audio-handler.component.ts
@@ -1,210 +1,175 @@
 import {Component, OnInit} from '@angular/core';
 import {SignalRService} from "../service/signalRService";
-import {SoundTouch} from "soundtouch-ts";
 
 /**
  * The AudioHandlerComponent represents a component that handles audio playback and buffering.
  */
 @Component({
-  selector: 'app-audio-handler',
-  templateUrl: './audio-handler.component.html',
-  styleUrls: ['./audio-handler.component.scss']
+    selector: 'app-audio-handler',
+    templateUrl: './audio-handler.component.html',
+    styleUrls: ['./audio-handler.component.scss']
 })
 export class AudioHandlerComponent implements OnInit {
 
-  // Constants for audio buffering and sampling
-  private readonly BUFFER_SIZE_IN_SECONDS = 30;
-  private readonly SAMPLE_RATE = 48000;
-  private readonly NUM_CHANNELS = 1;
+    // Constants for audio buffering and sampling
+    private readonly SAMPLE_RATE = 48000;
 
-  // Audio buffers and context
-  private audioContext = new AudioContext();
-  private audioBufferNode: AudioWorkletNode | undefined;
+    // Audio buffers and context
+    private audioContext = new AudioContext();
+    private audioBufferNode: AudioWorkletNode | undefined;
 
-  private skipSeconds = 5;
-  private playbackSpeed = 1;
-
-  private soundTouch = new SoundTouch(this.SAMPLE_RATE);
-
-  private gainNode: GainNode = this.audioContext.createGain();
-  private volume = 1;
-
-  private audioPlaying = false;
-
-  /**
-   * Gets the reference to the SignalRService.
-   * @param signalRService - The SignalRService to get the reference to.
-   */
-  constructor(private signalRService: SignalRService) {
-    this.audioContext.audioWorklet
-      .addModule('/assets/worklets/circular-buffer-worklet.js')
-      .catch((err) => {
-        console.error("Error loading worklet: " + err)
-      })
-      .then(() => {
-        this.audioBufferNode = new AudioWorkletNode(this.audioContext, 'circular-buffer-worklet')
-        this.audioBufferNode.connect(this.gainNode);
-        this.gainNode.connect(this.audioContext.destination);
-      })
-  }
-
-  /**
-   * Initializes Services and audio nodes
-   */
-  ngOnInit(): void {
-    // Subscribe to the received audio stream event from SignalRService
-    this.signalRService.receivedAudioStream.subscribe((newChunk) => {
-      this.handleAudioData(newChunk)
-    });
-  }
-
-  /**
-   * Resumes audio playback and starts the source node if not started.
-   */
-  public togglePlayback(): void {
-    if (!this.audioPlaying) {
-      this.audioContext.resume().then(() => {
-        this.audioPlaying = true;
-      })
-    } else {
-      this.audioContext.suspend().then(() => {
-        this.audioPlaying = false;
-      })
-    }
-  }
+    private skipSeconds = 5;
+    private playbackSpeed = 1;
 
 
-  /**
-   * Handles the received audio data chunk.
-   * @param newChunk - The new audio chunk received.
-   */
-  private handleAudioData(newChunk: Int16Array): void {
-    // Convert the received Int16Array chunk to Float32Array
-    const convertedAudioData: Float32Array = new Float32Array(newChunk.length);
-    const INT16_TO_FLOAT32_SCALING_FACTOR = 32767;
+    private gainNode: GainNode = this.audioContext.createGain();
+    private volume = 1;
 
-    for (let i = 0; i < newChunk.length; i++) {
-      convertedAudioData[i] = newChunk[i] / INT16_TO_FLOAT32_SCALING_FACTOR;  // Skalierung auf den Bereich von -1 bis 1
+    private audioPlaying = false;
+
+    /**
+     * Gets the reference to the SignalRService and Initializes the AudioWorklet.
+     * @param signalRService - The SignalRService to get the reference to.
+     */
+    constructor(private signalRService: SignalRService) {
+        this.audioContext.audioWorklet
+            .addModule('/assets/worklets/circular-buffer-worklet.js')
+            .catch((err) => {
+                console.error("Error loading worklet: " + err)
+            })
+            .then(() => {
+                this.audioBufferNode = new AudioWorkletNode(this.audioContext, 'circular-buffer-worklet')
+                this.audioBufferNode.connect(this.gainNode);
+                this.gainNode.connect(this.audioContext.destination);
+            })
     }
 
-    const WORKLET_FRAME_SIZE = 128;
-    const ITERATIONS_NEEDED_FOR_FULL_SECOND = this.SAMPLE_RATE / WORKLET_FRAME_SIZE;
-
-    for (let i = 0; i < ITERATIONS_NEEDED_FOR_FULL_SECOND; i++) {
-      this.audioBufferNode?.port.postMessage(convertedAudioData.subarray(i * WORKLET_FRAME_SIZE, (i + 1) * WORKLET_FRAME_SIZE));
+    /**
+     * Initializes SignalR stream connection.
+     */
+    ngOnInit(): void {
+        // Subscribe to the received audio stream event from SignalRService
+        this.signalRService.receivedAudioStream.subscribe((newChunk) => {
+            this.handleAudioData(newChunk)
+        });
     }
-  }
 
-  // /**
-  //  * Replaces current playing audio node with audio node containing new data
-  //  * Each audio node holds 1s of audio
-  //  */
-  // private updateAudioNode(): void {
-  //   if (!this.audioPlaying) return;
-  //
-  //   const audioData: Float32Array | null = this.audioBuffer.readNextSecond();
-  //
-  //   if (!audioData || audioData.length === 0) return;
-  //
-  //   const audioDataWithPlaybackSpeed: Float32Array = this.adjustAudioDataForPlaybackSpeed(audioData);
-  //
-  //   const audioBuffer: AudioBuffer = this.audioContext.createBuffer(this.NUM_CHANNELS, audioDataWithPlaybackSpeed.length, this.SAMPLE_RATE);
-  //   const SELECTED_CHANNEL = 0;
-  //   const BUFFER_OFFSET = 0;
-  //
-  //   audioBuffer.copyToChannel(audioDataWithPlaybackSpeed, SELECTED_CHANNEL, BUFFER_OFFSET)
-  //
-  //   const audioNode: AudioBufferSourceNode = new AudioBufferSourceNode(this.audioContext, {buffer: audioBuffer});
-  //
-  //   // Connect the source node to the audio context destination
-  //   audioNode.connect(this.gainNode)
-  //   audioNode.start(0);
-  //   this.currentAudioNode?.disconnect();
-  //   this.currentAudioNode = audioNode;
-  // }
-
-  private adjustAudioDataForPlaybackSpeed(audioData: Float32Array): Float32Array {
-    this.soundTouch.tempo = this.playbackSpeed;
-    this.soundTouch.inputBuffer.putSamples(audioData);
-    this.soundTouch.process();
-
-    const timeShiftedAudioData = new Float32Array(this.SAMPLE_RATE / this.playbackSpeed);
-
-    this.soundTouch.outputBuffer.receiveSamples(timeShiftedAudioData, this.soundTouch.outputBuffer.frameCount);
-    this.soundTouch.clear();
-
-    return timeShiftedAudioData;
-  }
-
-  /**
-   * Sets the playback speed of the audio.
-   * @param speed - The playback speed to set.
-   */
-  public setPlaybackSpeed(speed: number): void {
-    this.playbackSpeed = speed;
-  }
+    /**
+     * Resumes audio playback and starts the source node if not started.
+     */
+    public togglePlayback(): void {
+        if (!this.audioPlaying) {
+            this.audioContext.resume().then(() => {
+                this.audioPlaying = true;
+                this.audioBufferNode?.port.postMessage({type: "play"});
+            })
+        } else {
+            this.audioContext.suspend().then(() => {
+                this.audioPlaying = false;
+                this.audioBufferNode?.port.postMessage({type: "pause"});
+            })
+        }
+    }
 
 
-  /**
-   * Skips forward in the audio playback by the specified number of seconds.
-   */
-  public skipForward(): void {
-    // this.audioBuffer.advanceReadPointer(this.skipSeconds);
-  }
+    /**
+     * Converts AudioChunks to Float32 and sends them to AudioWorklet.
+     * @param newChunk - The new audio chunk received.
+     */
+    private handleAudioData(newChunk: Int16Array): void {
+        // Convert the received Int16Array chunk to Float32Array
+        const convertedAudioData: Float32Array = new Float32Array(newChunk.length);
+        const INT16_TO_FLOAT32_SCALING_FACTOR = 32767;
 
-  /**
-   * Skips backward in the audio playback by the specified number of seconds.
-   */
-  public skipBackward(): void {
-    // this.audioBuffer.decreaseReadPointer(this.skipSeconds);
-  }
+        for (let i = 0; i < newChunk.length; i++) {
+            convertedAudioData[i] = newChunk[i] / INT16_TO_FLOAT32_SCALING_FACTOR;  // Skalierung auf den Bereich von -1 bis 1
+        }
 
-  /**
-   * Sets the volume of the audio.
-   * @param volume - The volume level to set.
-   */
-  public setVolume(volume: number): void {
-    const LOWEST_INPUT_VOL_LEVEL = -1;
-    const HIGHEST_INPUT_VOL_LEVEL = 1;
+        const WORKLET_FRAME_SIZE = 128;
+        const ITERATIONS_NEEDED_FOR_FULL_SECOND = this.SAMPLE_RATE / WORKLET_FRAME_SIZE;
 
-    const MAX_VOLUME = 1.5;
-    const MIN_VOLUME = 0;
+        for (let i = 0; i < ITERATIONS_NEEDED_FOR_FULL_SECOND; i++) {
+            this.audioBufferNode?.port.postMessage({
+                type: "audioData",
+                audioData: convertedAudioData.subarray(i * WORKLET_FRAME_SIZE, (i + 1) * WORKLET_FRAME_SIZE)
+            });
+        }
+    }
 
-    this.volume = this.mapVolumeToNewScale(volume, LOWEST_INPUT_VOL_LEVEL, HIGHEST_INPUT_VOL_LEVEL, MIN_VOLUME, MAX_VOLUME);
 
-    if (!this.gainNode) return;
-    this.gainNode.gain.setValueAtTime(this.volume, this.audioContext.currentTime);
-  }
+    /**
+     * Sets the playback speed of the audio.
+     * @param speed - The playback speed to set.
+     */
+    public setPlaybackSpeed(speed: number): void {
+        this.playbackSpeed = speed;
+    }
 
-  /**
-   * Maps number to new range according to original range given
-   * @param value Number that's to be mapped
-   * @param origMin Lowest value of original range
-   * @param origMax Highest value of original range
-   * @param newMin Lowest value of new range
-   * @param newMax Highest value of new range
-   */
-  private mapVolumeToNewScale(value: number, origMin: number, origMax: number, newMin: number, newMax: number): number {
-    return (value - origMin) * (newMax - newMin) / (origMax - origMin) + newMin;
-  }
 
-  public getVolume(): number {
-    return this.volume;
-  }
+    /**
+     * Skips forward in the audio playback by the specified number of seconds.
+     */
+    public skipForward(): void {
+        this.audioBufferNode?.port.postMessage({type: "skipForward", seconds: this.skipSeconds});
+    }
 
-  /**
-   * Sets amount of seconds to be skipped
-   * @param seconds
-   */
-  public setSkipSeconds(seconds: number): void {
-    this.skipSeconds = seconds;
-  }
+    /**
+     * Skips backward in the audio playback by the specified number of seconds.
+     */
+    public skipBackward(): void {
+        this.audioBufferNode?.port.postMessage({type: "skipBackward", seconds: this.skipSeconds})
+    }
 
-  /**
-   * Returns the current status of the audio playback.
-   * @returns {boolean} True if audio is currently playing, false otherwise.
-   */
-  public getIsAudioPlaying(): boolean {
-    return this.audioPlaying;
-  }
+    /**
+     * Sets the volume of the audio.
+     * @param volume - The volume level to set.
+     */
+    public setVolume(volume: number): void {
+        const LOWEST_INPUT_VOL_LEVEL = -1;
+        const HIGHEST_INPUT_VOL_LEVEL = 1;
+
+        const MAX_VOLUME = 1.5;
+        const MIN_VOLUME = 0;
+
+        this.volume = this.mapVolumeToNewScale(volume, LOWEST_INPUT_VOL_LEVEL, HIGHEST_INPUT_VOL_LEVEL, MIN_VOLUME, MAX_VOLUME);
+
+        if (!this.gainNode) return;
+        this.gainNode.gain.setValueAtTime(this.volume, this.audioContext.currentTime);
+    }
+
+    /**
+     * Maps number to new range according to original range given
+     * @param value Number that's to be mapped
+     * @param origMin Lowest value of original range
+     * @param origMax Highest value of original range
+     * @param newMin Lowest value of new range
+     * @param newMax Highest value of new range
+     */
+    private mapVolumeToNewScale(value: number, origMin: number, origMax: number, newMin: number, newMax: number): number {
+        return (value - origMin) * (newMax - newMin) / (origMax - origMin) + newMin;
+    }
+
+    /**
+     * Returns the current volume level.
+     * @returns {number} The current volume level.
+     */
+    public getVolume(): number {
+        return this.volume;
+    }
+
+    /**
+     * Sets amount of seconds to be skipped
+     * @param {number} seconds The amount of seconds to be skipped
+     */
+    public setSkipSeconds(seconds: number): void {
+        this.skipSeconds = seconds;
+    }
+
+    /**
+     * Returns the current status of the audio playback.
+     * @returns {boolean} True if audio is currently playing, false otherwise.
+     */
+    public getIsAudioPlaying(): boolean {
+        return this.audioPlaying;
+    }
 }
--- src/assets/worklets/circular-buffer-worklet.js
+++ src/assets/worklets/circular-buffer-worklet.js
@@ -1,9 +1,12 @@
 class CircularBufferWorklet extends AudioWorkletProcessor {
+  // Audio Buffer related fields
   buffer;
   totalBufferSize;
-
   samplingRate;
   bufferLengthInSeconds;
+
+  // Playback
+  audioPlaying;
 
   // Actual pointers used for data access in the circular buffer itself
   writePointer;
@@ -23,11 +26,11 @@ class CircularBufferWorklet extends AudioWorkletProcessor {
   constructor() {
     // Worklet initialization
     super();
-    this.port.onmessage = (e) => this.handleNewAudioData(e.data);
+    this.port.onmessage = (e) => this.handlePortMessage(e.data);
 
     // Buffer parameters
     const SAMPLE_RATE = 48000;
-    const BUFFER_LENGTH_IN_SECONDS = 10;
+    const BUFFER_LENGTH_IN_SECONDS = 30;
     const SAFETY_MARGIN_IN_SECONDS = 2;
 
     // Circular buffer initialization
@@ -35,6 +38,7 @@ class CircularBufferWorklet extends AudioWorkletProcessor {
     this.bufferLengthInSeconds = BUFFER_LENGTH_IN_SECONDS;
     this.safetyMarginInSeconds = SAFETY_MARGIN_IN_SECONDS;
 
+    this.audioPlaying = false;
     this.writePointer = 0;
     this.readPointer = 0;
     this.absoluteReadTimeInSeconds = 0;
@@ -43,6 +47,28 @@ class CircularBufferWorklet extends AudioWorkletProcessor {
     this.audioChunksRead = 0;
     this.totalBufferSize = this.samplingRate * this.bufferLengthInSeconds;
     this.buffer = new Float32Array(this.totalBufferSize);
+  }
+
+  handlePortMessage(message) {
+    switch (message.type) {
+      case 'audioData':
+        this.handleNewAudioData(message.audioData);
+        break;
+      case 'skipForward':
+        this.advanceReadPointer(message.seconds)
+        break;
+      case 'skipBackward':
+        this.decreaseReadPointer(message.seconds)
+        break;
+      case 'pause':
+        this.audioPlaying = false;
+        break;
+      case 'play':
+        this.audioPlaying = true;
+        break;
+      default:
+        console.log("Unknown message type!");
+    }
   }
 
   handleNewAudioData(audioChunk) {
@@ -65,6 +91,10 @@ class CircularBufferWorklet extends AudioWorkletProcessor {
   }
 
   process(inputs, outputs, parameters) {
+    if (!this.audioPlaying) {
+      return true;
+    }
+
     const outputChannel = outputs[0][0];
 
     const FRAME_SIZE = outputChannel.length; // Should be 128
@@ -118,6 +148,41 @@ class CircularBufferWorklet extends AudioWorkletProcessor {
       return Float32Array.of(...FIRST_HALF, ...SECOND_HALF)
     } else {
       return this.buffer.subarray(oldReadPointer, newReadPointer);
+    }
+  }
+
+  /**
+   * Advances the read pointer by the given amount of seconds.
+   * Used for skipping forward in the audio playback.
+   * @param secondsToAdvance - The amount of seconds to advance the read pointer by.
+   */
+  advanceReadPointer(secondsToAdvance) {
+    const SAMPLES_TO_ADVANCE = secondsToAdvance * this.samplingRate;
+    this.readPointer = (this.readPointer + SAMPLES_TO_ADVANCE) % this.totalBufferSize;
+    this.absoluteReadTimeInSeconds += secondsToAdvance;
+
+    // Keep Read Pointer away from Write Pointer
+    if (this.absoluteReadTimeInSeconds >= this.absoluteWriteTimeInSeconds) {
+      this.absoluteReadTimeInSeconds = this.absoluteWriteTimeInSeconds - this.safetyMarginInSeconds;
+      this.readPointer = this.writePointer - (this.safetyMarginInSeconds * this.samplingRate);
+    }
+  }
+
+  /**
+   * Decreases the read pointer by the given amount of seconds.
+   * Used for skipping backward in the audio playback.
+   * @param secondsToDecrease - The amount of seconds to decrease the read pointer by.
+   */
+  decreaseReadPointer(secondsToDecrease) {
+    const SAMPLES_TO_ADVANCE = secondsToDecrease * this.samplingRate;
+    this.readPointer = (this.readPointer - SAMPLES_TO_ADVANCE) % this.totalBufferSize;
+    this.absoluteReadTimeInSeconds -= secondsToDecrease;
+
+    // Keep Read Pointer away from End of Buffer
+    const END_OF_BUFFER_TIME_TIMESTAMP = this.absoluteWriteTimeInSeconds - this.bufferLengthInSeconds;
+    if (this.absoluteReadTimeInSeconds <= END_OF_BUFFER_TIME_TIMESTAMP) {
+      this.absoluteReadTimeInSeconds = END_OF_BUFFER_TIME_TIMESTAMP + this.safetyMarginInSeconds;
+      this.readPointer = this.writePointer - this.totalBufferSize + (this.safetyMarginInSeconds * this.samplingRate);
     }
   }
 }
