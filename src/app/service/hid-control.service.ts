7cb13fe2137fc6ce1f4c7a3859399368b50c653f
cneidahl <christoph.neidahl@stud.htwk-leipzig.de>
2023-11-17 22:09:42 +0100

    #46 fix: Use @types/w3c-web-hid

--- src/app/service/hid-control.service.ts
+++ src/app/service/hid-control.service.ts
@@ -1,17 +1,5 @@
 import {Injectable} from '@angular/core';
 
-interface HidDetails {
-  vendorId: number;
-  productId: number;
-  usagePage: number;
-  usage: number;
-}
-
-interface HidDevice extends HidDetails {
-  open(): void;
-  addEventListener(eventName: string, event: any): void;
-}
-
 /**
  * Service to request & configure access to external HID control devices (foot control, hand control, etc)
  */
@@ -20,7 +8,7 @@ interface HidDevice extends HidDetails {
 })
 export class HidControlService {
 
-  private HID_DEVICES: HidDetails[] = [
+  private HID_DEVICES: HIDDeviceFilter[] = [
     {
       // Grundig foot control (USB)
       vendorId: 0x15d8,
@@ -34,14 +22,14 @@ export class HidControlService {
   private forFunc: () => void = () => null;
   private revFunc: () => void = () => null;
 
-  private lastState: number = 0;
+  private lastState = 0;
 
   /**
    * Initializes the dictionary with default values.
    */
   constructor() {
     if (!this.isSupportedWebHID()) {
-      console.error('WebHID is not supported in this browser, you cannot make use of external control devices.');
+      console.error("WebHID is not supported in this browser, you cannot make use of external control devices.");
       return;
     }
   }
@@ -56,9 +44,9 @@ export class HidControlService {
     this.revFunc = revFunc;
   }
 
-  private async findAllowedDevices(): Promise<HidDevice[]> {
-    const webhid = (navigator as any).hid;
-    const allowedDevices: HidDevice[] = await webhid.getDevices();
+  private async findAllowedDevices(): Promise<HIDDevice[]> {
+    const webhid = navigator.hid;
+    const allowedDevices: HIDDevice[] = await webhid.getDevices();
     return allowedDevices.filter (allowedDevice =>
       this.HID_DEVICES.find (handlableDevice =>
         handlableDevice.vendorId === allowedDevice.vendorId
@@ -67,13 +55,13 @@ export class HidControlService {
 
   public async configureDevices() {
     if (!this.isSupportedWebHID()) return;
-    const webhid = (navigator as any).hid;
+    const webhid = navigator.hid;
 
-    let allowedDevices: HidDevice[] = await this.findAllowedDevices();
+    let allowedDevices: HIDDevice[] = await this.findAllowedDevices();
     console.log("Currently allowed devices:");
     console.log(allowedDevices);
 
-    const devicesNotYetAllowed: HidDetails[] = this.HID_DEVICES.filter (handlableDevice =>
+    const devicesNotYetAllowed: HIDDeviceFilter[] = this.HID_DEVICES.filter (handlableDevice =>
       allowedDevices.find (allowedDevice =>
         handlableDevice.vendorId === allowedDevice.vendorId
         && handlableDevice.productId === allowedDevice.productId) === undefined);
@@ -82,9 +70,13 @@ export class HidControlService {
       console.log("Devices we could additionally handle:");
       console.log(devicesNotYetAllowed);
 
-      await webhid.requestDevice({
-        filters:devicesNotYetAllowed
-      });
+      try {
+        await webhid.requestDevice({
+          filters:devicesNotYetAllowed
+        });
+      } catch (e) {
+        console.error ("Unable to request device access");
+      }
 
       // get new list, in case more devices were allowed
       allowedDevices = await this.findAllowedDevices();
@@ -100,13 +92,13 @@ export class HidControlService {
         return;
       }
 
-      allowedDevice.addEventListener("inputreport", (event: any) => {
+      allowedDevice.addEventListener("inputreport", (event) => {
         const { data } = event;
 
         const value: number = data.getUint8(0);
 
         // decypher button states for printing
-        let valueMeaning: string = "stop";
+        let valueMeaning = "stop";
         if (value > 0) {
           valueMeaning = "";
           let valueCopy: number = value;
