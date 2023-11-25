b984c2464239d4b80e8cec50e0a8317beb57adb6
cneidahl <christoph.neidahl@stud.htwk-leipzig.de>
2023-11-25 15:36:20 +0100

    #70 feat: Re-prettify hid-control service

--- src/app/service/hid-control.service.ts
+++ src/app/service/hid-control.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@angular/core'
+import { Injectable } from '@angular/core';
 
 /**
  * Service to request & configure access to external HID control devices (foot control, hand control, etc)
@@ -16,10 +16,10 @@ export class HidControlService {
       usagePage: 0xffff,
       usage: 0x01,
     },
-  ]
+  ];
 
   // The previous switch state, required for i.e. differenciating stop->play and play+ffwd->play
-  private lastState = 0
+  private lastState = 0;
 
   /**
    * Does an initial check for WebHID support, issues one error at the start if WebHID is unsupported in this environment.
@@ -28,8 +28,8 @@ export class HidControlService {
     if (!this.isSupportedWebHID()) {
       console.error(
         'WebHID is not supported in this browser, you cannot make use of external control devices.',
-      )
-      return
+      );
+      return;
     }
   }
 
@@ -37,14 +37,14 @@ export class HidControlService {
    * Check for WebHID support
    */
   private isSupportedWebHID(): boolean {
-    return 'hid' in navigator
+    return 'hid' in navigator;
   }
 
   /**
    * Find devices we've previously been granted access to, and filter out devices we can't actually handle
    */
   private async findAllowedDevices(): Promise<HIDDevice[]> {
-    const allowedDevices: HIDDevice[] = await navigator.hid.getDevices()
+    const allowedDevices: HIDDevice[] = await navigator.hid.getDevices();
     return allowedDevices.filter(
       (allowedDevice) =>
         this.HID_DEVICES.find(
@@ -52,7 +52,7 @@ export class HidControlService {
             handlableDevice.vendorId === allowedDevice.vendorId &&
             handlableDevice.productId === allowedDevice.productId,
         ) !== undefined,
-    )
+    );
   }
 
   /**
@@ -61,10 +61,10 @@ export class HidControlService {
    */
   private async checkForDevices(): Promise<void> {
     // get permitted devices we care about
-    const alreadyAllowedDevices: HIDDevice[] = await this.findAllowedDevices()
+    const alreadyAllowedDevices: HIDDevice[] = await this.findAllowedDevices();
     console.log(
       `We have been granted access to ${alreadyAllowedDevices.length} devices`,
-    )
+    );
 
     // find out what devices we have yet to be granted permission to
     const unhandledDevices: HIDDeviceFilter[] = this.HID_DEVICES.filter(
@@ -74,19 +74,19 @@ export class HidControlService {
             potentialDevice.vendorId === allowedDevice.vendorId &&
             potentialDevice.productId === allowedDevice.productId,
         ) === undefined,
-    )
+    );
     console.log(
       `Of those device(s), we don't yet have permissions to access ${unhandledDevices.length} of them`,
-    )
-    if (unhandledDevices.length === 0) return
+    );
+    if (unhandledDevices.length === 0) return;
 
     // try to request access to them
     try {
       await navigator.hid.requestDevice({
         filters: unhandledDevices,
-      })
+      });
     } catch (e) {
-      console.error('Unable to request device access')
+      console.error('Unable to request device access');
     }
   }
 
@@ -99,58 +99,58 @@ export class HidControlService {
     callbackRewind: () => void,
   ): (ev: HIDInputReportEvent) => void {
     return (event) => {
-      const { data } = event
+      const { data } = event;
 
-      const REWIND_BIT = 1 << 2
-      const PLAY_BIT = 1 << 1
-      const FASTFORWARD_BIT = 1 << 0
-      const value: number = data.getUint8(0)
+      const REWIND_BIT = 1 << 2;
+      const PLAY_BIT = 1 << 1;
+      const FASTFORWARD_BIT = 1 << 0;
+      const value: number = data.getUint8(0);
 
       // decypher button states for printing
-      let valueMeaning = 'stop'
+      let valueMeaning = 'stop';
       if (value > 0) {
-        const meaningsSet: string[] = []
-        if ((value & REWIND_BIT) !== REWIND_BIT) meaningsSet.push('rewind')
-        if ((value & PLAY_BIT) !== PLAY_BIT) meaningsSet.push('play')
+        const meaningsSet: string[] = [];
+        if ((value & REWIND_BIT) !== REWIND_BIT) meaningsSet.push('rewind');
+        if ((value & PLAY_BIT) !== PLAY_BIT) meaningsSet.push('play');
         if ((value & FASTFORWARD_BIT) !== FASTFORWARD_BIT)
-          meaningsSet.push('fast-forward')
-        valueMeaning = meaningsSet.toString()
+          meaningsSet.push('fast-forward');
+        valueMeaning = meaningsSet.toString();
       }
-      console.log(`pedal says: ${value} (meaning: ${valueMeaning}`)
+      console.log(`pedal says: ${value} (meaning: ${valueMeaning}`);
 
       // decide what to do, based on current & last button states
       switch (value) {
         // nothing pressed, stop
         case 0:
-          if (this.lastState === PLAY_BIT) callbackPlay()
-          break
+          if (this.lastState === PLAY_BIT) callbackPlay();
+          break;
 
         // only play pressed
         case PLAY_BIT:
-          if (this.lastState === 0) callbackPlay()
-          break
+          if (this.lastState === 0) callbackPlay();
+          break;
 
         // fast-forward pressed, with or without play
         case FASTFORWARD_BIT:
         case FASTFORWARD_BIT + PLAY_BIT:
-          callbackFastforward()
-          break
+          callbackFastforward();
+          break;
 
         // rewind pressed, with or without play
         case REWIND_BIT:
         case REWIND_BIT + PLAY_BIT:
-          callbackRewind()
-          break
+          callbackRewind();
+          break;
 
         // anything else, no clue how to respond to it
         default:
-          console.log("Don't know what to do in this state")
-          break
+          console.log("Don't know what to do in this state");
+          break;
       }
 
       // save this state for following calls
-      this.lastState = value
-    }
+      this.lastState = value;
+    };
   }
 
   /**
@@ -161,32 +161,32 @@ export class HidControlService {
     callbackFastforward: () => void,
     callbackRewind: () => void,
   ) {
-    if (!this.isSupportedWebHID()) return
+    if (!this.isSupportedWebHID()) return;
 
-    await this.checkForDevices()
+    await this.checkForDevices();
 
-    const allowedDevices: HIDDevice[] = await this.findAllowedDevices()
-    console.log('Currently allowed devices:')
-    console.log(allowedDevices)
+    const allowedDevices: HIDDevice[] = await this.findAllowedDevices();
+    console.log('Currently allowed devices:');
+    console.log(allowedDevices);
 
     const inputCallback = this.makeCallbackDeviceInput(
       callbackPlay,
       callbackFastforward,
       callbackRewind,
-    )
+    );
 
     allowedDevices.forEach(async (allowedDevice) => {
       try {
-        await allowedDevice.open()
+        await allowedDevice.open();
       } catch (e) {
         console.log(
           `Failed to open device ${allowedDevice.vendorId}:${allowedDevice.productId}:`,
           (e as Error).message,
-        )
-        return
+        );
+        return;
       }
 
-      allowedDevice.addEventListener('inputreport', inputCallback)
-    })
+      allowedDevice.addEventListener('inputreport', inputCallback);
+    });
   }
 }
